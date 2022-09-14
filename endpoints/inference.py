a8cdd755482cb8b75245715d68a7869cc7ea01b7
Daniel Szemerey <daniel@szemerey.eu>
2022-09-14 15:49:54 +0200

    feat: Added support for multiple models.

--- endpoints/inference.py
+++ endpoints/inference.py
@@ -3,15 +3,20 @@ import pandas as pd
 import ast
 from typing import Tuple
 import random
+from mopi.blocks.pipeline import Pipeline
+from mopi.blocks.io import load_pipeline
+from mopi.inference import run_inference
+from mopi.library.experiments.hate_speech import random, all_0s, all_1s
+from mopi.constants import Const
+import json
 
-class RandomModel:
-    def infer(self, text: str)->Tuple[int, list[float]]:
-        random_category = random.randint(0, 1)
-        return random_category,[0.1,0.0] if random_category == 0 else [0.0,1.0]
+from os import listdir
+from os.path import isfile, join
+
 
 class ModelEndpoint(Resource):
-    def __init__(self)->None:
-        self.model = RandomModel()
+    def __init__(self) -> None:
+        self.pipelines:dict[str, Pipeline] = {"random":random, "all_0s": all_0s, "all_1s":all_1s}
     
     def load(self)->None:
         pass
@@ -20,14 +25,32 @@ class ModelEndpoint(Resource):
         parser = reqparse.RequestParser()  # initialize
         
         parser.add_argument('text', required=True, location='args')  # add args
+        parser.add_argument('pipeline_name', required=True, location='args')  # add args
         
         args = parser.parse_args()  # parse arguments to dictionary
    
-        queried_text = args['text']
+        queried_texts = args['text'].split(";")
+        pipeline_name = args['pipeline_name']
         
-        result = self.model.infer(queried_text)
+        available_models =  [f.replace(".pth","") for f in listdir(Const.output_pipelines_path) if isfile(join(Const.output_pipelines_path, f))] + list(self.pipelines.keys())
+        
+        if pipeline_name not in available_models:
+            return {
+                'message': f"{pipeline_name} is not available."
+            }, 409 
+        
+        if pipeline_name in self.pipelines.keys():
+            print("getting it from dict")
+            pipeline = self.pipelines[pipeline_name]
+        else:
+            print("Loading it")
+            pipeline = load_pipeline(pipeline_name)
+            self.pipelines[pipeline_name] = pipeline
+        
+        
+        results = run_inference(pipeline, queried_texts)
+   
+        predictions = {queried_texts[i]: str(result) for i, result in enumerate(results)}
 
-
-
-        return {'result': result}, 200  # return data with 200 OK 
+        return {'result': predictions}, 200  # return data with 200 OK 
                
