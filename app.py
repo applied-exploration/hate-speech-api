8d14491f17e7b190a11acb996c9f49a9d49a8099
Daniel Szemerey <daniel@szemerey.eu>
2022-09-14 16:10:45 +0200

    chore: Took out another folder.

--- app.py
+++ app.py
@@ -8,15 +8,3 @@ api = Api(app)
 
 
 api.add_resource(ModelEndpoint, '/detect') 
-
-
-# if __name__ == '__main__':
-#     app.run()  # run our Flask app
-    
-
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello Sammy!'
