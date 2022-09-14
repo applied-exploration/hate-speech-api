cbae66ef3e1a4e38d4e3547668529baa9b17f198
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-09-14 17:44:40 +0200

    chore: Without mopi.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_restful import Api
 
-from endpoints.inference import ModelEndpoint
+# from endpoints.inference import ModelEndpoint
 
 app = Flask(__name__)
 api = Api(app)
 
 
-api.add_resource(ModelEndpoint, '/detect') 
+# api.add_resource(ModelEndpoint, '/detect') 
